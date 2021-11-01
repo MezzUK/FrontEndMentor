1b10d04977b4f30dd709712b626bfb5ceae66403
MezzUK <bbadcock_uk@yahoo.com>
2021-11-01 12:32:42 +0000

    Active status

--- TimeTracking/scripts/scripts.js
+++ TimeTracking/scripts/scripts.js
@@ -1,33 +1,31 @@
-// var timesheetCard = document.getElementsByClassName('card__timesheet');
-// var timesheetHeaderTitle = document.getElementsByClassName('timesheet__details__title');
-// var timesheetHeaderImage = document.getElementsByClassName('card__timesheet__header__image');
-// var timesheetTime = document.getElementsByClassName('timesheet__details__time');
-// var timesheetPrevious = document.getElementsByClassName('timesheet__details__previous');
+let timeSpan = 'weekly';
 
 let requestFile = 'data/data.json'
 
 let request = new XMLHttpRequest();
 
-request.open('GET', requestFile);
+var timesheets = '';
 
-request.responseType = 'json';
-request.send();
+readJSON(requestFile);
 
 request.onload = function(){    
-    var timesheets = request.response;
+    timesheets = request.response;
+    console.log(timesheets);
+    showTimeSheets(timesheets);
+}
 
+function showTimeSheets(timesheets){
     for (let index = 0; index < timesheets.length; index++) {
-        //const element = timesheets[index];
         const myArticle = document.createElement("article");
         myArticle.classList.add("card__timesheet");
-        myArticle.style.setProperty('background-color', 'var(--' + timesheets[index].title.toLowerCase() + ')');
+        myArticle.style.backgroundColor = 'var(--' + timesheets[index].title.toLowerCase().replace(" ", "-") + ')';
         
         const timeSheetHeader = document.createElement("div")
         timeSheetHeader.classList.add("card__timesheet__header");
 
         const timeSheetHeaderImage = document.createElement("img");
         timeSheetHeaderImage.classList.add("card__timesheet__header__image");
-        timeSheetHeaderImage.src = "images/icon-" + timesheets[index].title + ".svg";
+        timeSheetHeaderImage.src = "images/icon-" + timesheets[index].title.replace(" ", "-") + ".svg";
         
         const timeSheetDetails = document.createElement("div");
         timeSheetDetails.classList.add("timesheet__details");
@@ -38,7 +36,7 @@ request.onload = function(){
 
         const timeSheetDetailsTime = document.createElement("div");
         timeSheetDetailsTime.classList.add("timesheet__details__time");
-        timeSheetDetailsTime.innerHTML = timesheets[index].timeframes['weekly'].current + "hrs";
+        timeSheetDetailsTime.innerHTML = timesheets[index].timeframes[timeSpan].current + "hrs";
 
         const timeSheetDetailsElipses = document.createElement("div");
         timeSheetDetailsElipses.classList.add("timesheet__details__elipses");
@@ -46,7 +44,20 @@ request.onload = function(){
 
         const timeSheetDetailsPrevious = document.createElement("div");
         timeSheetDetailsPrevious.classList.add("timesheet__details__previous");
-        timeSheetDetailsPrevious.innerHTML = "Last Week - " + timesheets[index].timeframes['weekly'].previous + "hrs";
+        var period;
+        switch(timeSpan){
+            case "daily":
+                period = "Yesterday - ";
+                break;
+            case "weekly":
+                period = "Last Week - ";
+                break;
+            case "monthly":
+                period = "Last Month - ";
+                break;
+        }
+
+        timeSheetDetailsPrevious.innerHTML = period + timesheets[index].timeframes[timeSpan].previous + "hrs";
         
         myArticle.appendChild(timeSheetHeader);
         timeSheetHeader.appendChild(timeSheetHeaderImage);
@@ -58,6 +69,27 @@ request.onload = function(){
 
         const footer = document.getElementsByClassName("attribution")[0];
         document.body.insertBefore(myArticle, footer);
-        
     }
+}
+
+function updateTimeSheets(){
+    var cards = document.getElementsByClassName("card__timesheet");
+    while(cards.length > 0){
+        cards[0].parentNode.removeChild(cards[0]);
+    }
+    showTimeSheets(timesheets);
+}
+
+function filterTime(time){
+    timeSpan = time;
+    document.getElementsByClassName("active")[0].classList.remove("active");
+    document.getElementsByName(timeSpan)[0].classList.add("active");
+    updateTimeSheets();
+    console.log(timeSpan)
+}
+
+function readJSON(requestFile){
+    request.open('GET', requestFile);
+    request.responseType = 'json';
+    request.send();
 }
